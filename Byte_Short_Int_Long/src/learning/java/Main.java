cb068ebc0e25821645640bb94f26a5ac76e3cd8e
Adrian Karwowski <adrian.karwowski@netguru.com>
2022-10-17 22:56:04 +0200

    Repo Check

---
+++ Byte_Short_Int_Long/src/learning/java/Main.java
@@ -0,0 +1,15 @@
+package learning.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 100000000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value:" + myMinIntValue);
+        System.out.println("Integer Maximum Value:" + myMaxIntValue);
+
+    }
+}
