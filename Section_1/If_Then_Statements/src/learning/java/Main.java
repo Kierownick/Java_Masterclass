77f2149df418f3ce3b71b25dfb78e1092a76b992
Adrian Karwowski <adrian.karwowski@netguru.com>
2022-10-18 16:49:16 +0200

    If Then Statement

---
+++ Section_1/If_Then_Statements/src/learning/java/Main.java
@@ -0,0 +1,14 @@
+package learning.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Conditional logic
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("It isn!");
+        }
+    }
+}
