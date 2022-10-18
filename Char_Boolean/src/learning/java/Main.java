fda27012560670806d5bc92b597db76b5907f064
Adrian Karwowski <adrian.karwowski@netguru.com>
2022-10-18 13:42:59 +0200

    Char and Boolean

---
+++ Char_Boolean/src/learning/java/Main.java
@@ -0,0 +1,20 @@
+package learning.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myCopyright = '\u00A9';
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myCopyright);
+
+        boolean myTrueBoolValue = true;
+        boolean myFalseBoolValue = false;
+
+        boolean isCustomerOver18 = true;
+    }
+}
