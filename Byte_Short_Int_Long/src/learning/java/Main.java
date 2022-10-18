707504f0897dd2c3cbac46f5e4d778860875c881
Adrian Karwowski <adrian.karwowski@netguru.com>
2022-10-18 09:53:59 +0200

    Byte short long double

--- Byte_Short_Int_Long/src/learning/java/Main.java
+++ Byte_Short_Int_Long/src/learning/java/Main.java
@@ -11,5 +11,30 @@ public class Main {
         System.out.println("Integer Minimum Value:" + myMinIntValue);
         System.out.println("Integer Maximum Value:" + myMaxIntValue);
 
+        //Overflow
+        System.out.println("Overflow int:" + (myMaxIntValue + 1));
+
+        //Underflow
+        System.out.println("Underflow int:" + (myMinIntValue - 1));
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Min byte number:" + myMinByteValue);
+        System.out.println("Max byte number:" + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Min short number:" + myMinShortValue);
+        System.out.println("Max short number:" + myMaxShortValue);
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Min long number:" + myMinLongValue);
+        System.out.println("Max long number:" + myMaxLongValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Min double number:" + myMinDoubleValue);
+        System.out.println("Max double number:" + myMaxDoubleValue);
     }
 }
