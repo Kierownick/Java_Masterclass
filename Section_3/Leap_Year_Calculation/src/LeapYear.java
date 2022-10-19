971e12c95fc4012210cb2fa554ca830d334f2cd1
Adrian Karwowski <adrian.karwowski@netguru.com>
2022-10-20 00:21:08 +0200

    Leap Year Calculation

---
+++ Section_3/Leap_Year_Calculation/src/LeapYear.java
@@ -0,0 +1,16 @@
+public class LeapYear {
+
+    public static boolean isLeapYear(int year) {
+
+        if (year < 1 || year > 9999) {
+            return false;
+
+        } else if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            }
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Section_3/Leap_Year_Calculation/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Leap year? " + LeapYear.isLeapYear(1600));
+        System.out.println("Leap year? " + LeapYear.isLeapYear(1800));
+
+    }
+}
