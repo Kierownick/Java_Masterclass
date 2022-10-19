8f5adbf50cbb452abbee1339a51641a67608c8a6
Adrian Karwowski <adrian.karwowski@netguru.com>
2022-10-19 23:25:31 +0200

    Barking Dog

---
+++ Section_3/Barking_Dog/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(BarkingDog.shouldWakeUp(true,23));
+        System.out.println(BarkingDog.shouldWakeUp(true,24));
+        System.out.println(BarkingDog.shouldWakeUp(false,6));
+        System.out.println(BarkingDog.shouldWakeUp(false,6));
+
+    }
+}
