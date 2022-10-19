d93bdba1c7dd633ea1f5fb0186e18ca2a00f1d80
Adrian Karwowski <adrian.karwowski@netguru.com>
2022-10-19 21:50:45 +0200

    Speed Converter

---
+++ Section_3/Speed_Converter/src/SpeedConverter.java
@@ -0,0 +1,22 @@
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            return Math.round(kilometersPerHour / 1.609d);
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " +
+                    milesPerHour + " mi/h");
+        }
+    }
+}
