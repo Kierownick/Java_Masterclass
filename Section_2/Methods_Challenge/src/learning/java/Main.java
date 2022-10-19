4959f23cff970b5c56fec74f06c65ff9e455c05d
Adrian Karwowski <adrian.karwowski@netguru.com>
2022-10-19 04:25:08 +0200

    Update Main.java

--- Section_2/Methods_Challenge/src/learning/java/Main.java
+++ Section_2/Methods_Challenge/src/learning/java/Main.java
@@ -26,9 +26,9 @@ public class Main {
 
         if (playerScore >= 1000) {
             return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
+        } else if (playerScore >= 500) {
             return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
+        } else if (playerScore >= 100) {
             return 3;
         } else return 4;
     }
