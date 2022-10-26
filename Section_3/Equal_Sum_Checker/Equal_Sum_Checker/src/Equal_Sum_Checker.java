b69ec9e15e9a3ad64566bb57a02ab9b5c564da75
Adrian Karwowski <53815823+Kierownick@users.noreply.github.com>
2022-10-27 00:01:58 +0200

    Equal_Sum_Checker

---
+++ Section_3/Equal_Sum_Checker/Equal_Sum_Checker/src/Equal_Sum_Checker.java
@@ -0,0 +1,10 @@
+public class Equal_Sum_Checker {
+
+    public static boolean EqualSumChecker(int a, int b, int c){
+
+        if (a + b == c ){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Section_3/Equal_Sum_Checker/Equal_Sum_Checker/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean Equal = Equal_Sum_Checker.EqualSumChecker(3, 2, 3);
+        System.out.println(Equal);
+    }
+}
